53627c2346b80e37b7159bda34383a30352b01a0
jpd61 <jpd@dewoodyoil.com>
2020-08-08 13:44:08 -0500

    heroku setup

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://heroku_qf994l44:Monkey!232@ds245250.mlab.com:45250/heroku_qf994l44',
+  {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 module.exports = mongoose.connection;
